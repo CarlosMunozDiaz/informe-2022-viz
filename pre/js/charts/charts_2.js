4ca3f512ba1c9ca21f12f23f02bc5af17131c3ed
Carlos Munoz Diaz <carlos.munoz@cchs.csic.es>
2022-01-03 13:00:14 +0100

    Inicialización de nuevo repositorio tras error no detectado en repo previo

---
+++ pre/js/charts/charts_2.js
@@ -0,0 +1,416 @@
+//Desarrollo de las visualizaciones
+//import * as d3 from 'd3';
+//import { numberWithCommas2 } from './helpers';
+//import { getInTooltip, getOutTooltip, positionTooltip } from './modules/tooltip';
+import { setChartHeight } from '../modules/height';
+import { setChartCanvas, setChartCanvasImage } from '../modules/canvas-image';
+import { setRRSSLinks } from '../modules/rrss';
+import { setFixedIframeUrl } from './chart_helpers';
+
+export function initChart2_1(iframe, position) {
+    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
+    document.getElementById('title').textContent = 'Gráfico 1_1';
+    document.getElementById('subtitle').textContent = 'Subtítulo del 1_1';
+    document.getElementById('data-source').textContent = 'INE';
+    document.getElementById('data-note').textContent = 'Notas: Nada';
+
+    //Desarrollo del gráfico   
+
+
+    //Animación del gráfico
+    document.getElementById('replay').addEventListener('click', function() {
+        animateChart();
+    });
+
+    //Iframe
+    setFixedIframeUrl('titulo_grafico_1_1');
+
+    //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
+    setRRSSLinks('Título del gráfico 1.1');
+
+    //Captura de pantalla de la visualización
+    setChartCanvas();
+
+    let pngDownload = document.getElementById('pngImage');
+
+    pngDownload.addEventListener('click', function(){
+        setChartCanvasImage('titulo_grafico_1_1');
+    });
+
+    //Altura del frame
+    setChartHeight(iframe);
+}
+
+export function initChart2_2(iframe, position) {
+    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
+    document.getElementById('title').textContent = 'Gráfico 1_2';
+    document.getElementById('subtitle').textContent = 'Subtítulo del 1_2';
+    document.getElementById('data-source').textContent = 'INE';
+    document.getElementById('data-note').textContent = 'Notas: Nada';
+
+    //Desarrollo del gráfico   
+
+
+    //Animación del gráfico
+    document.getElementById('replay').addEventListener('click', function() {
+        animateChart();
+    });
+
+    //Iframe
+    setFixedIframeUrl('titulo_grafico_1_2');
+
+    //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
+    setRRSSLinks('Título del gráfico 1.2');
+
+    //Captura de pantalla de la visualización
+    setChartCanvas();
+
+    let pngDownload = document.getElementById('pngImage');
+
+    pngDownload.addEventListener('click', function(){
+        setChartCanvasImage('titulo_grafico_1_2');
+    });
+
+    //Altura del frame
+    setChartHeight(iframe);
+}
+
+export function initChart2_3(iframe, position) {
+    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
+    document.getElementById('title').textContent = 'Gráfico 1_3';
+    document.getElementById('subtitle').textContent = 'Subtítulo del 1_3';
+    document.getElementById('data-source').textContent = 'INE';
+    document.getElementById('data-note').textContent = 'Notas: Nada';
+
+    //Desarrollo del gráfico   
+
+
+    //Animación del gráfico
+    document.getElementById('replay').addEventListener('click', function() {
+        animateChart();
+    });
+
+    //Iframe
+    setFixedIframeUrl('titulo_grafico_1_3');
+
+    //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
+    setRRSSLinks('Título del gráfico 1.3');
+
+    //Captura de pantalla de la visualización
+    setChartCanvas();
+
+    let pngDownload = document.getElementById('pngImage');
+
+    pngDownload.addEventListener('click', function(){
+        setChartCanvasImage('titulo_grafico_1_3');
+    });
+
+    //Altura del frame
+    setChartHeight(iframe);
+}
+
+export function initChart2_4(iframe, position) {
+    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
+    document.getElementById('title').textContent = 'Gráfico 1_4';
+    document.getElementById('subtitle').textContent = 'Subtítulo del 1_4';
+    document.getElementById('data-source').textContent = 'INE';
+    document.getElementById('data-note').textContent = 'Notas: Nada';
+
+    //Desarrollo del gráfico   
+
+
+    //Animación del gráfico
+    document.getElementById('replay').addEventListener('click', function() {
+        animateChart();
+    });
+
+    //Iframe
+    setFixedIframeUrl('titulo_grafico_1_4');
+
+    //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
+    setRRSSLinks('Título del gráfico 1.4');
+
+    //Captura de pantalla de la visualización
+    setChartCanvas();
+
+    let pngDownload = document.getElementById('pngImage');
+
+    pngDownload.addEventListener('click', function(){
+        setChartCanvasImage('titulo_grafico_1_4');
+    });
+
+    //Altura del frame
+    setChartHeight(iframe);
+}
+
+export function initChart2_5(iframe, position) {
+    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
+    document.getElementById('title').textContent = 'Gráfico 2_1';
+    document.getElementById('subtitle').textContent = 'Subtítulo del 2_1';
+    document.getElementById('data-source').textContent = 'INE';
+    document.getElementById('data-note').textContent = 'Notas: Nada';
+
+    //Desarrollo del gráfico   
+
+
+    //Animación del gráfico
+    document.getElementById('replay').addEventListener('click', function() {
+        animateChart();
+    });
+
+    //Iframe
+    setFixedIframeUrl('titulo_grafico_2_1');
+
+    //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
+    setRRSSLinks('Título del gráfico 2_1');
+
+    //Captura de pantalla de la visualización
+    setChartCanvas();
+
+    let pngDownload = document.getElementById('pngImage');
+
+    pngDownload.addEventListener('click', function(){
+        setChartCanvasImage('titulo_grafico_2_1');
+    });
+
+    //Altura del frame
+    setChartHeight(iframe);
+}
+
+export function initChart2_6(iframe, position) {
+    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
+    document.getElementById('title').textContent = 'Gráfico 2_2';
+    document.getElementById('subtitle').textContent = 'Subtítulo del 2_2';
+    document.getElementById('data-source').textContent = 'INE';
+    document.getElementById('data-note').textContent = 'Notas: Nada';
+
+    //Desarrollo del gráfico   
+
+
+    //Animación del gráfico
+    document.getElementById('replay').addEventListener('click', function() {
+        animateChart();
+    });
+
+    //Iframe
+    setFixedIframeUrl('titulo_grafico_2_2');
+
+    //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
+    setRRSSLinks('Título del gráfico 2_2');
+
+    //Captura de pantalla de la visualización
+    setChartCanvas();
+
+    let pngDownload = document.getElementById('pngImage');
+
+    pngDownload.addEventListener('click', function(){
+        setChartCanvasImage('titulo_grafico_2_2');
+    });
+
+    //Altura del frame
+    setChartHeight(iframe);
+}
+
+export function initChart2_7(iframe, position) {
+    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
+    document.getElementById('title').textContent = 'Gráfico 2_3';
+    document.getElementById('subtitle').textContent = 'Subtítulo del 2_3';
+    document.getElementById('data-source').textContent = 'INE';
+    document.getElementById('data-note').textContent = 'Notas: Nada';
+
+    //Desarrollo del gráfico   
+
+
+    //Animación del gráfico
+    document.getElementById('replay').addEventListener('click', function() {
+        animateChart();
+    });
+
+    //Iframe
+    setFixedIframeUrl('titulo_grafico_2_3');
+
+    //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
+    setRRSSLinks('Título del gráfico 2_3');
+
+    //Captura de pantalla de la visualización
+    setChartCanvas();
+
+    let pngDownload = document.getElementById('pngImage');
+
+    pngDownload.addEventListener('click', function(){
+        setChartCanvasImage('titulo_grafico_2_3');
+    });
+
+    //Altura del frame
+    setChartHeight(iframe);
+}
+
+export function initChart2_8(iframe, position) {
+    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
+    document.getElementById('title').textContent = 'Gráfico 1_4';
+    document.getElementById('subtitle').textContent = 'Subtítulo del 1_4';
+    document.getElementById('data-source').textContent = 'INE';
+    document.getElementById('data-note').textContent = 'Notas: Nada';
+
+    //Desarrollo del gráfico   
+
+
+    //Animación del gráfico
+    document.getElementById('replay').addEventListener('click', function() {
+        animateChart();
+    });
+
+    //Iframe
+    setFixedIframeUrl('titulo_grafico_1_4');
+
+    //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
+    setRRSSLinks('Título del gráfico 1.4');
+
+    //Captura de pantalla de la visualización
+    setChartCanvas();
+
+    let pngDownload = document.getElementById('pngImage');
+
+    pngDownload.addEventListener('click', function(){
+        setChartCanvasImage('titulo_grafico_1_4');
+    });
+
+    //Altura del frame
+    setChartHeight(iframe);
+}
+
+export function initChart2_9(iframe, position) {
+    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
+    document.getElementById('title').textContent = 'Gráfico 2_1';
+    document.getElementById('subtitle').textContent = 'Subtítulo del 2_1';
+    document.getElementById('data-source').textContent = 'INE';
+    document.getElementById('data-note').textContent = 'Notas: Nada';
+
+    //Desarrollo del gráfico   
+
+
+    //Animación del gráfico
+    document.getElementById('replay').addEventListener('click', function() {
+        animateChart();
+    });
+
+    //Iframe
+    setFixedIframeUrl('titulo_grafico_2_1');
+
+    //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
+    setRRSSLinks('Título del gráfico 2_1');
+
+    //Captura de pantalla de la visualización
+    setChartCanvas();
+
+    let pngDownload = document.getElementById('pngImage');
+
+    pngDownload.addEventListener('click', function(){
+        setChartCanvasImage('titulo_grafico_2_1');
+    });
+
+    //Altura del frame
+    setChartHeight(iframe);
+}
+
+export function initChart2_10(iframe, position) {
+    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
+    document.getElementById('title').textContent = 'Gráfico 2_2';
+    document.getElementById('subtitle').textContent = 'Subtítulo del 2_2';
+    document.getElementById('data-source').textContent = 'INE';
+    document.getElementById('data-note').textContent = 'Notas: Nada';
+
+    //Desarrollo del gráfico   
+
+
+    //Animación del gráfico
+    document.getElementById('replay').addEventListener('click', function() {
+        animateChart();
+    });
+
+    //Iframe
+    setFixedIframeUrl('titulo_grafico_2_2');
+
+    //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
+    setRRSSLinks('Título del gráfico 2_2');
+
+    //Captura de pantalla de la visualización
+    setChartCanvas();
+
+    let pngDownload = document.getElementById('pngImage');
+
+    pngDownload.addEventListener('click', function(){
+        setChartCanvasImage('titulo_grafico_2_2');
+    });
+
+    //Altura del frame
+    setChartHeight(iframe);
+}
+
+export function initChart2_11(iframe, position) {
+    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
+    document.getElementById('title').textContent = 'Gráfico 2_3';
+    document.getElementById('subtitle').textContent = 'Subtítulo del 2_3';
+    document.getElementById('data-source').textContent = 'INE';
+    document.getElementById('data-note').textContent = 'Notas: Nada';
+
+    //Desarrollo del gráfico   
+
+
+    //Animación del gráfico
+    document.getElementById('replay').addEventListener('click', function() {
+        animateChart();
+    });
+
+    //Iframe
+    setFixedIframeUrl('titulo_grafico_2_3');
+
+    //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
+    setRRSSLinks('Título del gráfico 2_3');
+
+    //Captura de pantalla de la visualización
+    setChartCanvas();
+
+    let pngDownload = document.getElementById('pngImage');
+
+    pngDownload.addEventListener('click', function(){
+        setChartCanvasImage('titulo_grafico_2_3');
+    });
+
+    //Altura del frame
+    setChartHeight(iframe);
+}
+
+export function initChart2_12(iframe, position) {
+    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
+    document.getElementById('title').textContent = 'Gráfico 2_3';
+    document.getElementById('subtitle').textContent = 'Subtítulo del 2_3';
+    document.getElementById('data-source').textContent = 'INE';
+    document.getElementById('data-note').textContent = 'Notas: Nada';
+
+    //Desarrollo del gráfico   
+
+
+    //Animación del gráfico
+    document.getElementById('replay').addEventListener('click', function() {
+        animateChart();
+    });
+
+    //Iframe
+    setFixedIframeUrl('titulo_grafico_2_3');
+
+    //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
+    setRRSSLinks('Título del gráfico 2_3');
+
+    //Captura de pantalla de la visualización
+    setChartCanvas();
+
+    let pngDownload = document.getElementById('pngImage');
+
+    pngDownload.addEventListener('click', function(){
+        setChartCanvasImage('titulo_grafico_2_3');
+    });
+
+    //Altura del frame
+    setChartHeight(iframe);
+}
---
+++ pre/js/main.js
@@ -0,0 +1,186 @@
+import 'url-search-params-polyfill';
+import './modules/tabs';
+import * as charts_1 from './charts/charts_1';
+import * as charts_2 from './charts/charts_2';
+import * as charts_3 from './charts/charts_3';
+import * as charts_4 from './charts/charts_4';
+
+//Necesario para importar los estilos de forma automática en la etiqueta 'style' del html final
+import '../css/main.scss';
+
+/////////////
+//////// RECOGIDA DE PARÁMETROS DE LA URL
+/////////////
+getUrlParams();
+
+function getUrlParams() {
+    const params = new URLSearchParams(window.location.search);
+
+    //Visualización y tipo de iframe
+    const viz = params.get('chart');
+    const iframe = params.get('iframe');
+
+    setChart(viz, iframe);
+}
+
+function setChart(viz, iframe) {
+    pymChild.onMessage('viewport-iframe-position', onScroll);
+
+    function onScroll(parentInfo) {
+        let steps = parentInfo.split(" ");
+
+        if(steps[2] != 0 && !isChartActive) {
+            switch(viz){
+                //Demografía
+                case '1_1':
+                    charts_1.initChart1_1(iframe, steps[2]);
+                    break;
+                case '1_2':
+                    charts_1.initChart1_2(iframe, steps[2]);
+                    break;
+                case '1_3':
+                    charts_1.initChart1_3(iframe, steps[2]);
+                    break;
+                case '1_4':
+                    charts_1.initChart1_4(iframe, steps[2]);
+                    break;
+                case '1_5':
+                    charts_1.initChart1_5(iframe, steps[2]);
+                    break;
+                case '1_6':
+                    charts_1.initChart1_6(iframe, steps[2]);
+                    break;
+                case '1_7':
+                    charts_1.initChart1_7(iframe, steps[2]);
+                    break;
+                case '1_8':
+                    charts_1.initChart1_8(iframe, steps[2]);
+                    break;
+                case '1_9':
+                    charts_1.initChart1_9(iframe, steps[2]);
+                    break;
+                case '1_10':
+                    charts_1.initChart1_10(iframe, steps[2]);
+                    break;
+                case '1_11':
+                    charts_1.initChart1_11(iframe, steps[2]);
+                    break;
+                //Social
+                case '2_1':
+                    charts_2.initChart2_1(iframe, steps[2]);
+                    break;
+                case '2_2':
+                    charts_2.initChart2_2(iframe, steps[2]);
+                    break;
+                case '2_3':
+                    charts_2.initChart2_3(iframe, steps[2]);
+                    break;
+                case '2_4':
+                    charts_2.initChart2_4(iframe, steps[2]);
+                    break;
+                case '2_5':
+                    charts_2.initChart2_5(iframe, steps[2]);
+                    break;
+                case '2_6':
+                    charts_2.initChart2_6(iframe, steps[2]);
+                    break;
+                case '2_7':
+                    charts_2.initChart2_7(iframe, steps[2]);
+                    break;
+                case '2_8':
+                    charts_2.initChart2_8(iframe, steps[2]);
+                    break;
+                case '2_9':
+                    charts_2.initChart2_9(iframe, steps[2]);
+                    break;
+                case '2_10':
+                    charts_2.initChart2_10(iframe, steps[2]);
+                    break;
+                case '2_11':
+                    charts_2.initChart2_11(iframe, steps[2]);
+                    break;
+                case '2_12':
+                    charts_2.initChart2_12(iframe, steps[2]);
+                    break;
+                //Economía
+                case '3_1':
+                    charts_3.initChart3_1(iframe, steps[2]);
+                    break;
+                case '3_2':
+                    charts_3.initChart3_2(iframe, steps[2]);
+                    break;
+                case '3_3':
+                    charts_3.initChart3_3(iframe, steps[2]);
+                    break;
+                case '3_4':
+                    charts_3.initChart3_4(iframe, steps[2]);
+                    break;
+                case '3_5':
+                    charts_3.initChart3_5(iframe, steps[2]);
+                    break;
+                case '3_6':
+                    charts_3.initChart3_6(iframe, steps[2]);
+                    break;
+                case '3_7':
+                    charts_3.initChart3_7(iframe, steps[2]);
+                    break;
+                case '3_8':
+                    charts_3.initChart3_8(iframe, steps[2]);
+                    break;
+                case '3_9':
+                    charts_3.initChart3_9(iframe, steps[2]);
+                    break;
+                case '3_10':
+                    charts_3.initChart3_10(iframe, steps[2]);
+                    break;
+                //Social
+                case '4_1':
+                    charts_4.initChart4_1(iframe, steps[2]);
+                    break;
+                case '4_2':
+                    charts_4.initChart4_2(iframe, steps[2]);
+                    break;
+                case '4_3':
+                    charts_4.initChart4_3(iframe, steps[2]);
+                    break;
+                case '4_4':
+                    charts_4.initChart4_4(iframe, steps[2]);
+                    break;
+                case '4_5':
+                    charts_4.initChart4_5(iframe, steps[2]);
+                    break;
+                case '4_6':
+                    charts_4.initChart4_6(iframe, steps[2]);
+                    break;
+                case '4_7':
+                    charts_4.initChart4_7(iframe, steps[2]);
+                    break;
+                case '4_8':
+                    charts_4.initChart4_8(iframe, steps[2]);
+                    break;
+                case '4_9':
+                    charts_4.initChart4_9(iframe, steps[2]);
+                    break;
+                case '4_10':
+                    charts_4.initChart4_10(iframe, steps[2]);
+                    break;
+                case '4_11':
+                    charts_4.initChart4_11(iframe, steps[2]);
+                    break;
+                case '4_12':
+                    charts_4.initChart4_12(iframe, steps[2]);
+                    break;
+                case '4_13':
+                    charts_4.initChart4_13(iframe, steps[2]);
+                    break;
+                case '4_14':
+                    charts_4.initChart4_14(iframe, steps[2]);
+                    break;
+                default: //Si no hay parámetros en la URL
+                    charts_1.initChart1_1('responsive', steps[2]);
+                        break;                
+            }
+            isChartActive = true;
+        }
+    }    
+}
